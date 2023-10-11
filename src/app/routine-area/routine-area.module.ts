6a0796aa2c35a224da9de3b5dd2a46f2462d5fcc
DarthIV02 <ivannia.gomez@cetys.edu.mx>
2023-10-11 16:08:55 -0700

    Uncomment pop-up routine

--- src/app/routine-area/routine-area.module.ts
+++ src/app/routine-area/routine-area.module.ts
@@ -7,12 +7,12 @@ import { BlockComponentComponent } from '../block-component/block-component.comp
 import { PopUpComponent } from '../pop-up/pop-up.component';
 import { PopUpSaveComponent } from '../pop-up-save/pop-up-save.component';
 import { PopUpClearComponent } from '../pop-up-clear/pop-up-clear.component';
-//import { PopUpRoutinesComponent } from '../pop-up-routines/pop-up-routines.component';
+import { PopUpRoutinesComponent } from '../pop-up-routines/pop-up-routines.component';
 import { PopUpLoadPreviousRoutineComponent } from '../pop-up-load-previous-routine/pop-up-load-previous-routine.component';
 
 @NgModule({
   declarations: [BlockComponentComponent, PopUpComponent, PopUpSaveComponent, 
-    PopUpClearComponent, PopUpLoadPreviousRoutineComponent],
+    PopUpClearComponent, PopUpLoadPreviousRoutineComponent, PopUpRoutinesComponent],
   imports: [
     CommonModule,
     IonicModule,
@@ -24,6 +24,7 @@ import { PopUpLoadPreviousRoutineComponent } from '../pop-up-load-previous-routi
     PopUpSaveComponent,
     PopUpClearComponent,
     PopUpLoadPreviousRoutineComponent,
+    PopUpRoutinesComponent,
   ],
 })
 export class RoutineAreaModule { }
